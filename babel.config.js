8f5767d33db33d324a8d12a1335b499c327571f1
曹文学 <caowx@pkusoft.net>
2021-09-30 18:28:33 +0800

    生产环境移除console

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
+const prodPlugins = []
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push([
+    'transform-remove-console',
+    {
+      // 保留 console.error 与 console.warn
+      exclude: ['error', 'warn']
+    }
+  ])
+}
 console.log(1)
 module.exports = {
   presets: [
@@ -13,6 +23,7 @@ module.exports = {
           return `element-plus/packages/theme-chalk/src/${name}.scss`
         }
       }
-    ]
+    ],
+    ...prodPlugins
   ]
 }
