9380f6e265a7ae3c3cd88e8f79cea869311804d9
曹文学 <caowx@pkusoft.net>
2021-11-18 18:34:58 +0800

    修改

--- src/router/router.fullPage.js
+++
@@ -1,8 +0,0 @@
-export const fullPageRouter = [
-    {
-        path: '/login',
-        name: 'login',
-        component: () => import('@/views/fullScreen/login/index.vue'),
-        meta: { open: true, title: '登录', titleEn: 'Login' }
-    }
-]
