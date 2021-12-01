61002ed30b5e83d8eb357a3f713060a62cfb1363
曹文学 <caowx@pkusoft.net>
2021-12-01 16:43:36 +0800

    图形验证组件

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -31,15 +31,22 @@
                 <p class="login-tips">Tips : [账号: test, 密码: 123456]</p>
             </el-form>
         </div>
+        <!-- 图形验证组件 -->
+        <Vcode :show="vCodeShow" @success="onSuccess" @close="onClose"/>
     </div>
 </template>
 
 <script>
     import { setToken } from '@/util/cookies.js'
+    import Vcode from 'vue3-puzzle-vcode'
     export default {
         name: 'login-page',
+        components: {
+            Vcode
+        },
         data: function () {
             return {
+                vCodeShow: false,
                 formData: {
                     username: '',
                     password: ''
@@ -54,86 +61,36 @@
             submitForm () {
                 this.$refs['login-form'].validate(valid => {
                     if (valid) {
-                        this.$store.commit('SET_FULL_LOADING', { isLoading: true })
-                        this.$axios({
-                            url: this.$api.user.login,
-                            method: 'post',
-                            data: this.formData
-                        }).then(res => {
-                            this.$store.commit('SET_FULL_LOADING', { isLoading: false })
-                            if (res.data.code === 's00') {
-                                setToken(res.data.token)
-                                this.$router.push({ name: 'dashboard' })
-                            } else {
-                                this.$message.warning(res.data.msg)
-                            }
-                        }).catch(() => {
-                            this.$store.commit('SET_FULL_LOADING', { isLoading: false })
-                            this.$message.error('未知错误，请稍后重试')
-                        })
+                        this.vCodeShow = true
                     } else {
                         return false
                     }
                 })
+            },
+            onSuccess () {
+                this.$store.commit('SET_FULL_LOADING', { isLoading: true })
+                this.$axios({
+                    url: this.$api.user.login,
+                    method: 'post',
+                    data: this.formData
+                }).then(res => {
+                    this.$store.commit('SET_FULL_LOADING', { isLoading: false })
+                    if (res.data.code === 's00') {
+                        setToken(res.data.token)
+                        this.$router.push({ name: 'dashboard' })
+                    } else {
+                        this.$message.warning(res.data.msg)
+                    }
+                }).catch(() => {
+                    this.$store.commit('SET_FULL_LOADING', { isLoading: false })
+                    this.$message.error('未知错误，请稍后重试')
+                })
+            },
+            onClose () {
+                this.vCodeShow = false
             }
         }
     }
 </script>
 
-<style lang="scss">
-.page-login {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    background-image: url(./assets/images/login-bg.jpg);
-    background-size: 100% 100%;
-
-    .login-box {
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        width: 350px;
-        margin: -190px 0 0 -175px;
-        border-radius: 5px;
-        background: rgba(255, 255, 255, 0.3);
-        overflow: hidden;
-
-        .login-title {
-            width: 100%;
-            line-height: 50px;
-            text-align: center;
-            font-size: 20px;
-            color: #0066d0;
-            border-bottom: 1px solid #ddd;
-        }
-
-        .login-form {
-            padding: 30px 30px;
-
-            .el-form-item {
-                margin-bottom: 18px;
-
-                .el-button {
-                    margin-left: -8px;
-                }
-            }
-
-            .login-btn {
-                text-align: center;
-
-                button {
-                    width: 100%;
-                    height: 36px;
-                    margin-bottom: 10px;
-                }
-            }
-
-            .login-tips {
-                font-size: 12px;
-                line-height: 30px;
-                color: #555;
-            }
-        }
-    }
-}
-</style>
+<style lang="scss" src="./assets/css/index.scss"></style>
